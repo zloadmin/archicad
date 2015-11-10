28517f42a8a6fc25e4766483b533aa619bd28dbe
zloadmin <ded@xakep.ru>
2015-11-10 19:02:58 +0500

    send to server

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -76,9 +76,9 @@ class FilesController extends Controller
 
         $comand = 'wine '.base_path().'/autocad/LP_XMLConverter.exe libpart2xml -l UTF8 "'.$win_gdl_file.'" "'.$win_xml_file.'"';
 
-        passthru ($comand, $return);
 
-        dd($return);
+
+        dd(system($comand));
         // 4. Проверяем наличе нужных переменных
 
 
