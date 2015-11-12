4dec519c667e81b18a3e46d268b9ec97496d3fe1
zloadmin <ded@xakep.ru>
2015-11-12 13:18:47 +0500

    bag fix

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -144,7 +144,7 @@ class FilesController extends Controller
         if($file) {
             $auth = Files::is_auth($request->input('iduser'), $request->input('hash'));
             if(!$auth) return "Авторизация не пройдена";
-            return Files::get_file($file, $request->input('iduser');
+            return Files::get_file($file, $request->input('iduser'));
         } else {
             return "Файл не найден";
         }
